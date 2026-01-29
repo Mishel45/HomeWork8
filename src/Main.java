5671fc7b43e66b50c5b23be442df5aa4e3a21be6
Mishel45 <mikhail-pavlov1980@mail.ru>
2026-01-29 23:41:33 +0300

    delete misteike

--- src/Main.java
+++ src/Main.java
@@ -10,43 +10,36 @@ public class Main {
         for (int i = 1; i < 11; i++) {
             System.out.println(i);
         }
-
         System.out.println();
         System.out.println("Задача №2 ");
         for (int i = 10; i > 0; i--) {
             System.out.println(i);
         }
-
         System.out.println();
         System.out.println("Задача №3 ");
         for (int i = 0; i < 17; i = i + 2) {
             System.out.println(i);
         }
-
         System.out.println();
         System.out.println("Задача №4 ");
         for (int i = 10; i > -11; i--) {
             System.out.println(i);
         }
-
         System.out.println();
         System.out.println("Задача №5 ");
         for (int i = 1904; i < 2097; i = i + 4) {
             System.out.println(i + " год является високосным");
         }
-
         System.out.println();
         System.out.println("Задача №6 ");
         for (int i = 7; i < 99; i = i + 7) {
             System.out.println(i);
         }
-
         System.out.println();
         System.out.println("Задача №7 ");
         for (int i = 1; i < 513; i = i * 2) {
             System.out.println(i);
         }
-
         System.out.println();
         System.out.println("Задача №8 ");
         int cash = 0;
@@ -54,7 +47,6 @@ public class Main {
             cash = cash + 29000;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + cash + " рублей");
         }
-
         System.out.println();
         System.out.println("Задача №9 ");
         cash = 0;
@@ -62,7 +54,6 @@ public class Main {
             cash = cash + 29000 + cash / 100;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + cash + " рублей");
         }
-
         System.out.println();
         System.out.println("Задача №10 ");
         int number = 2;
